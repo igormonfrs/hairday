c0bfdfeadc7f7d54f0fe95f31274d834d23f3787
Igor <reisigorak@gmail.com>
2024-09-27 16:34:22 -0300

    applying packages

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict"
+
+// Configuração do dayjs
+import "./libs/dayjs.js"
+
+// CSS
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/schedule.css"
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CoypeWebpackPlugin = require("copy-webpack-plugin")
+
+module.exports = {
+  target: "web",
+  mode: "development",
+
+  entry: path.resolve(__dirname, "src", "main.js"),
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    liveReload: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+      favicon: path.resolve("src", "assets", "scissors.svg"),
+    }),
+
+    new CoypeWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets"),
+          to: path.resolve(__dirname, "dist", "src", "assets"),
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+        exclude: "/node_modules",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+}
