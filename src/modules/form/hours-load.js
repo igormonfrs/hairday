57b43a2770f38d31f808094ec8e85b50f9913026
Igor <reisigorak@gmail.com>
2024-09-27 20:56:54 -0300

    redering hours in page

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from "dayjs"
 import { openingHours } from "../../utils/opening-hours.js"
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad({ date }) {
   const opening = openingHours.map((hour) => {
     // Recupera somente a hora
@@ -16,5 +18,13 @@ export function hoursLoad({ date }) {
     }
   })
 
-  console.log(opening)
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li")
+
+    li.classList.add("hour")
+    li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+    li.textContent = hour
+    hours.append(li)
+  })
 }
